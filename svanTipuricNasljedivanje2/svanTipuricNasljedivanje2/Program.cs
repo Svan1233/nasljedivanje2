4e2c6e82bd62fc9ad15bfffbe261b81b1154d616
Svan1233 <55134376+Svan1233@users.noreply.github.com>
2020-10-13 10:37:10 +0200

    Add files via upload

---
+++ svanTipuricNasljedivanje2/svanTipuricNasljedivanje2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace svanTipuricNasljedivanje2
+{
+    class Program
+    {
+        interface IControl
+        {
+            void Metoda();
+            int Brojac { get; set; }
+
+        }
+        class ComboBox : IControl
+        {
+            private int brojac;
+
+            public int Brojac { get => brojac; set => brojac = value; }
+            public void Metoda()
+            {
+                Console.WriteLine("Metoda kombo");
+            }
+        }
+        class ListBox : IControl
+        {
+            private int brojac;
+
+            public int Brojac { get => brojac; set => brojac = value; }
+
+            public void Metoda()
+            {
+                Console.WriteLine("Metoda lista");
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            
+            ComboBox y = new ComboBox();
+            ListBox x = new ListBox();
+            x.Metoda();
+            y.Metoda();
+            Console.ReadKey();
+        }
+    }
+}
